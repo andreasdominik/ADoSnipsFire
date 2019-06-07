1cc260293676de500c3ec67ae222ab8a49957149
andreasdominik <andreas.dominik@mni.thm.de>
2019-06-07 18:32:53 +0200

    api impl

--- Skill/api.jl
+++ Skill/api.jl
@@ -2,3 +2,36 @@
 # API function goes here, to be called by the
 # skill-actions:
 #
+
+ADB = "$(Snips.getAppDir())/Skill/fire.sh"
+
+
+
+
+
+
+function amazonON()
+    adbCmds("wake")
+end
+
+function amazonOFF()
+    adbCmds("sleep")
+end
+
+function amazonARD()
+    adbCmds("ard")
+end
+
+function amazonZDF()
+    adbCmds("zdf")
+end
+
+
+
+
+function adbCmds(cmds)
+
+    return tryrun(`$ADB $(split(cmds))`, errorMsg =
+    """An error occured while sending commands $cmds
+    to Amazon fire."""
+end
